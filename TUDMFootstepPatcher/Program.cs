d1658c79f1e422518287d90edb61742ec2eafae2
cuyima <vivilotta98@live.de>
2020-12-13 16:57:55 +0100

    don't patch unplayable armor

--- TUDMFootstepPatcher/Program.cs
+++ TUDMFootstepPatcher/Program.cs
@@ -12,19 +12,32 @@ namespace TUDMFootstepPatcher
     {
 
         static ModKey ultimateDodgeMod = ModKey.FromNameAndExtension("Ultimate Dodge Mod.esp");
+        static FormKey[] playableRaces = new FormKey[]{
+                Skyrim.Race.DefaultRace,
+                Skyrim.Race.NordRace,
+                Skyrim.Race.BretonRace,
+                Skyrim.Race.RedguardRace,
+                Skyrim.Race.ImperialRace,
+                Skyrim.Race.DarkElfRace,
+                Skyrim.Race.HighElfRace,
+                Skyrim.Race.WoodElfRace,
+                Skyrim.Race.OrcRace,
+                Skyrim.Race.KhajiitRace,
+                Skyrim.Race.ArgonianRace,
+            };
         public static int Main(string[] args)
         {
-                return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
-                args,
-                RunPatch,
-                new UserPreferences
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+            args,
+            RunPatch,
+            new UserPreferences
+            {
+                ActionsForEmptyArgs = new RunDefaultPatcher
                 {
-                    ActionsForEmptyArgs = new RunDefaultPatcher
-                    {
-                        IdentifyingModKey = "TUDMFootstepPatch.esp",
-                        TargetRelease = GameRelease.SkyrimSE
-                    }
-                });
+                    IdentifyingModKey = "TUDMFootstepPatch.esp",
+                    TargetRelease = GameRelease.SkyrimSE
+                }
+            });
 
         }
 
@@ -57,37 +70,35 @@ namespace TUDMFootstepPatcher
             {
                 Console.WriteLine("CHECKING ARMOR " + armor.EditorID);
                 if (armor.Keywords == null) continue;
-                if (armor.Keywords.Contains(Skyrim.Keyword.ArmorCuirass) || armor.Keywords.Contains(Skyrim.Keyword.ClothingBody))
+                if (!armor.Keywords.Contains(Skyrim.Keyword.ArmorCuirass) && !armor.Keywords.Contains(Skyrim.Keyword.ClothingBody))
+                    continue;
+                foreach (var armature in armor.Armature)
                 {
-                    foreach (var armature in armor.Armature) {
-                        armature.TryResolve(state.LinkCache, out var armorAddonVar);
-                        if (armorAddonVar == null) continue;
-                        else
-                        {
-                            ArmorAddon armorAddon = armorAddonVar.DeepCopy();
-                            Console.WriteLine("CHECKING ARMATURE " + armorAddon.EditorID);
-                            if (HasBodyFlag(armorAddon))
-                            {
-                                Console.WriteLine("ARMATURE HAS BODYFLAG");
-                                switch(armor.DeepCopy().BodyTemplate!.ArmorType)
-                                {
-                                    case ArmorType.Clothing:
-                                        armorAddon.FootstepSound = UDRollNakedLandingSet.DeepCopy();
-                                        break;
-                                    case ArmorType.LightArmor:
-                                        armorAddon.FootstepSound = UDRollLightLandingSet.DeepCopy();
-                                        break;
-                                    case ArmorType.HeavyArmor:
-                                        armorAddon.FootstepSound = UDRollHeavyLandingSet.DeepCopy();
-                                        break;
-                                }
-                                state.PatchMod.ArmorAddons.Set(armorAddon);
-                            }
-                            else continue;
-                        }
+                    armature.TryResolve(state.LinkCache, out var armorAddonVar);
+                    if (armorAddonVar == null) continue;
+                    var race = armorAddonVar.Race;
+                    race.TryResolve(state.LinkCache, out var raceVar);
+                    if (raceVar == null || !playableRaces.Contains<FormKey>(raceVar.FormKey)) continue;
+
+                    ArmorAddon armorAddon = armorAddonVar.DeepCopy();
+                    Console.WriteLine("CHECKING ARMATURE " + armorAddon.EditorID);
+                    if (!HasBodyFlag(armorAddon)) continue;
+                    Console.WriteLine("ARMATURE HAS BODYFLAG");
+                    switch (armor.DeepCopy().BodyTemplate!.ArmorType)
+                    {
+                        case ArmorType.Clothing:
+                            armorAddon.FootstepSound = UDRollNakedLandingSet.DeepCopy();
+                            break;
+                        case ArmorType.LightArmor:
+                            armorAddon.FootstepSound = UDRollLightLandingSet.DeepCopy();
+                            break;
+                        case ArmorType.HeavyArmor:
+                            armorAddon.FootstepSound = UDRollHeavyLandingSet.DeepCopy();
+                            break;
                     }
+                    state.PatchMod.ArmorAddons.Set(armorAddon);
                 }
-                else continue;
+
             }
         }
     }
